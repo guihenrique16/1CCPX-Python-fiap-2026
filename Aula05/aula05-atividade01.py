033a20ef63b27bcf8e6951000c06f5a8d366fc6d
guihenrique16 <guilherme97035@gmail.com>
2026-04-30 22:27:42 -0300

    aula05

---
+++ Aula05/aula05-atividade01.py
@@ -0,0 +1,5 @@
+nomes=['a', 'm', 'v', 't']
+
+for i in range(len(nomes)):
+    for j in range(i+1, len(nomes)):
+        print(nomes[i], nomes[j])
---
+++ Aula05/aula05-matrizes.py
@@ -0,0 +1,10 @@
+musicas = [
+    ['this love', 'm5'],
+    ['boate azul', 'bm'],
+    ['policia', 'l']
+]
+
+for musica in musicas:
+    for info in musica:
+        print(info)
+    print()
